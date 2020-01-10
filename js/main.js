4873f9141c4a93862272c6eb5897f68324131a1c
Kathleen Wang <kathleen.wangibm.com@kathleens-mbp.cambridge.ibm.com>
2020-01-10 17:40:12 -0500

    files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
   
-
- let cards = ["queen", "queen", "king","king"]; 
+ 
+ let cards =  [{
+	rank: "queen",
+	suit: "hearts",
+	cardImg: "images/queen-of-hearts.png"  },
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImg: "images/queen-of-diamonds.png"  },
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImg: "images/king-of-hearts.png"  },
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImg: "images/king-of-diamonds.png"  }
+	]
 
  let cardsInPlay = []
  
@@ -8,20 +24,31 @@
  function checkForMatch() {
  if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
+  
 } else {
   console.log("Sorry, try again.");
 }
+cardsInPlay = [] 
+ 
 }
 
-function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID])
-	cardsInPlay.push(cards[cardID])
+function flipCard() {
+	console.log("User flipped " + cards[this.id].rank)
+	cardsInPlay.push(cards[this.id].rank)
+	this.src = cards[this.id].cardImg
 
 	if (cardsInPlay.length == 2) {
  	checkForMatch()
+
+
+
  }
- 
+ } 
+
+// need to get length of list elements next time.. 
+for (let i =0;i<4;i++) {
+	document.getElementsByTagName('img')[i].addEventListener('click',flipCard)
+	document.getElementsByTagName('img')[i].setAttribute('id',i)
 }
-// next step is to have each image have an ID?? thing where if you click it, and see if the ID numbers
-flipCard(2) 
-flipCard(3)
+ 
+
